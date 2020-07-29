8c19686c663cc4b1313bcc301c9d1130792da5c9
leah-bommer <leah@bommer.io>
2020-07-29 13:37:20 -0700

    rewrote adjusted and get radius because they were convoluted. rewriting the circle animation, attempting to understand why 785 and 40 are used as contants so i can strip it out of the circle animation code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,40 @@
 
 var imgSequence = [
-    ['/img/flow/1-homeScreen.png'],
-    ['/img/flow/2-editBills.png']
+    ['/img/flow/1-homeScreen.png', [100, 200]],
+    ['/img/flow/2-editBills.png', [200, 300]]
 ];
+
+var currImgIndex = 0;
+
+// might need a rename, what this actually does is confusing
+function adjustedImageHeight () {
+    return ($('.screenshot').height()) / 785;
+}
+
+function getCircleRadius () {
+    return adjustedImageHeight() * 40;
+}
+
+// TODO remove 
+console.log('Adjusted Image Height ' + adjustedImageHeight());
+console.log('Circle Radius: ' + getCircleRadius());
+
+function animateCircle(circleRadius){
+    var radius = getCircleRadius();
+    var coordinates = imgSequence[currImgIndex][1];
+    console.log(coordinates);
+    var top = adjustedImageHeight(coordinates[1]) - radius;
+    console.log('Top: ' + top);
+
+    
+    $('.circle')
+        .animate({
+            //top: second index of [width, height], so height of image at current step, minus a circle radius with get circle radius return subtracted
+            top: adjustedImageHeight(coordinates[1]) - radius
+        });
+
+}
+
+console.log('coordinates of circle: ');
+animateCircle();
+console.log(' At step: ' + currImgIndex);
