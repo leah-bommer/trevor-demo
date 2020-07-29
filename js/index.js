b3a944c99de893b7052aef9ad582ab1cb116cb74
leah-bommer <leah@bommer.io>
2020-07-29 14:27:04 -0700

    rewrote circle radius, it was returning a string instead of a number. getImageHeight just returns the screenshot image height, im still not sure why they used what they did, might not need it. rewriting animateCircle as placeAndAnimateCircle, might change name. dont need to set width or height because its dependant on the vw and changes with the resizing of the whole viewport. i feel like they were doing all that unnessecarily but maybe theres a reason ill see further oon

--- js/index.js
+++ js/index.js
@@ -7,34 +7,34 @@ var imgSequence = [
 var currImgIndex = 0;
 
 // might need a rename, what this actually does is confusing
-function adjustedImageHeight () {
-    return ($('.screenshot').height()) / 785;
+// function adjustedImageHeight () {
+//     return ($('.screenshot').height()) / 785;
+// }
+
+function getImageHeight () {
+    return $('.screenshot').height();
 }
+
 
 function getCircleRadius () {
-    return adjustedImageHeight() * 40;
+    const selectCircle = document.querySelector('.circle');
+    const circleCSSProperties = getComputedStyle(selectCircle);
+    const height = parseFloat(circleCSSProperties.height);
+    return height / 2;
 }
 
-// TODO remove 
-console.log('Adjusted Image Height ' + adjustedImageHeight());
-console.log('Circle Radius: ' + getCircleRadius());
-
-function animateCircle(circleRadius){
+function placeAndAnimateCircle(){
     var radius = getCircleRadius();
     var coordinates = imgSequence[currImgIndex][1];
-    console.log(coordinates);
-    var top = adjustedImageHeight(coordinates[1]) - radius;
-    console.log('Top: ' + top);
+    console.log('Coordinates of circle: ' + coordinates);
+    console.log('With Radius: ' + radius);
+    console.log('At step: ' + currImgIndex);
 
-    
     $('.circle')
         .animate({
-            //top: second index of [width, height], so height of image at current step, minus a circle radius with get circle radius return subtracted
-            top: adjustedImageHeight(coordinates[1]) - radius
+            top: 34,
+            left: 100
         });
-
 }
 
-console.log('coordinates of circle: ');
-animateCircle();
-console.log(' At step: ' + currImgIndex);
+placeAndAnimateCircle();
