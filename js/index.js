2c18aa5e66098887e37560c9ca68dc55ee4a75da
leah-bommer <leah@bommer.io>
2020-07-30 13:49:12 -0700

    label appears and the label text associated with each image changes on click. currently fixed to the middle of the screen

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 // TODO next: move circle positioning relative to input components adjusted to size of the screenshot 
 
 var imgSequence = [
-    ['/img/flow/1-homeScreen.png', [0, 0]],
-    ['/img/flow/2-editBills.png', [0, 0]],
-    ['/img/flow/3-editBillsExpandedSettings.png', [0, 0]],
-    ['/img/flow/4-editBills.png', [0, 0]],
-    ['/img/flow/5-editBillsExpandedLevels.png', [0, 0]],
-    ['/img/flow/6-homeScreen.png', [0, 0]],
-    ['/img/flow/7-editParts.png', [0, 0]],
-    ['/img/flow/8-homeScreen.png', [0, 0]],
-    ['/img/flow/9-bommerTree.png', [0, 0]],
-    ['/img/flow/10-settingsScreen.png', [0, 0]],
-    ['/img/flow/11-bommerTree.png', [0, 0]],
-    ['/img/flow/12-exportScreen.png', [0, 0]],
-    ['/img/flow/13-bommerTree.png', [0, 0]]
+    ['/img/flow/1-homeScreen.png', [0, 0], 'This is the first label and I need length to test.'],
+    ['/img/flow/2-editBills.png', [0, 0], 'This is the second label and I need length to test.'],
+    ['/img/flow/3-editBillsExpandedSettings.png', [0, 0], 'This is the third label and I need length to test.'],
+    ['/img/flow/4-editBills.png', [0, 0], 'This is the fourth label and I need length to test.'],
+    ['/img/flow/5-editBillsExpandedLevels.png', [0, 0], 'This is the fifth label and I need length to test.'],
+    ['/img/flow/6-homeScreen.png', [0, 0], 'This is the sixth label and I need length to test.'],
+    ['/img/flow/7-editParts.png', [0, 0], 'This is the seventh label and I need length to test.'],
+    ['/img/flow/8-homeScreen.png', [0, 0], 'This is the eighth label and I need length to test.'],
+    ['/img/flow/9-bommerTree.png', [0, 0], 'This is the ninth label and I need length to test.'],
+    ['/img/flow/10-settingsScreen.png', [0, 0], 'This is the tenth label and I need length to test.'],
+    ['/img/flow/11-bommerTree.png', [0, 0], 'This is the eleventh label and I need length to test.'],
+    ['/img/flow/12-exportScreen.png', [0, 0], 'This is the twelf. twelveth. twweeeeeelffffft label and I need length to test.'],
+    ['/img/flow/13-bommerTree.png', [0, 0], 'This is the thirteenth label and I need length to test.']
 ];
 
 var currImgIndex = 0;
@@ -28,7 +28,10 @@ $('.circle').click(function() {
     else { currImgIndex = 0 }
     
     var currImage = imgSequence[currImgIndex][0];
-    document.getElementById("screenshotId").src= currImage;
+    document.getElementById("screenshot-id").src= currImage;
+
+    var currLabel = imgSequence[currImgIndex][2];
+    document.getElementById('circle-label-id').textContent = currLabel;
     
     //test
     console.log('AFTER: ' + currImgIndex, imgSequence.length, currImage);
