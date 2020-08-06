c507522a1039299d57a17f23c292013f8f3e75bc
leah-bommer <leah@bommer.io>
2020-08-06 13:02:26 -0700

    i have GOT to stop installing modules willy nilly before I commit. ugh. everything is working, if jest gets wonky the main cpoyovers are just the 3 index files. circle positioning on window resize and image change done.  Seperation of concerns implemented to my non repeating myself satisfaction.

--- js/index.js
+++ js/index.js
@@ -1,58 +1,92 @@
 // TODO next: move circle positioning relative to input components adjusted to size of the screenshot 
 
 var imgSequence = [
-    ['/img/flow/1-homeScreen.png', [0, 0], 'This is the first label and I need length to test.', [4, 2]],
-    ['/img/flow/2-editBills.png', [0, 0], 'This is the second label and I need length to test.', [3, 5]],
-    ['/img/flow/3-editBillsExpandedSettings.png', [0, 0], 'This is the third label and I need length to test.', [.3, 4]],
-    ['/img/flow/4-editBills.png', [0, 0], 'This is the fourth label and I need length to test.', [.4, .5]],
-    ['/img/flow/5-editBillsExpandedLevels.png', [0, 0], 'This is the fifth label and I need length to test.', [50, 100]],
-    ['/img/flow/6-homeScreen.png', [0, 0], 'This is the sixth label and I need length to test.', [600, 50]],
-    ['/img/flow/7-editParts.png', [0, 0], 'This is the seventh label and I need length to test.', [70, 5]],
-    ['/img/flow/8-homeScreen.png', [0, 0], 'This is the eighth label and I need length to test.', [8, 5]],
-    ['/img/flow/9-bommerTree.png', [0, 0], 'This is the ninth label and I need length to test.', [90, 5]],
-    ['/img/flow/10-settingsScreen.png', [0, 0], 'This is the tenth label and I need length to test.', [100, 5]],
-    ['/img/flow/11-bommerTree.png', [0, 0], 'This is the eleventh label and I need length to test.', [11, 5]],
-    ['/img/flow/12-exportScreen.png', [0, 0], 'This is the twelf. twelveth. twweeeeeelffffft label and I need length to test.', [12, 5]],
-    ['/img/flow/13-bommerTree.png', [0, 0], 'This is the thirteenth label and I need length to test.', [13 , 5]]
+    ['/img/flow/1-homeScreen.png', [0, 0], 'This is the first label and I need length to test.', [71.27, 15.55]],
+    ['/img/flow/2-editBills.png', [0, 0], 'This is the second label and I need length to test.', [15.58, 22.27]],
+    ['/img/flow/3-editBillsExpandedSettings.png', [0, 0], 'This is the third label and I need length to test.', [15.58, 22.27]],
+    ['/img/flow/4-editBills.png', [0, 0], 'This is the fourth label and I need length to test.', [16.60, 57.61]]
 ];
 
+var focusPoint = $('.focus-point');
 var currImgIndex = 0;
 var label = imgSequence[currImgIndex][2];
-var position = imgSequence[currImgIndex][3];
+var startPosition = imgSequence[currImgIndex][3];
+//set initial circle position
+focusPoint.css('left', calculateCoordinates()[0] + 'px');
+focusPoint.css('top', calculateCoordinates()[1] + 'px');
+//set initial label text
 document.getElementById('circle-label-text-id').textContent = label;
 
-$('.focus-point').click(function() {
-    
-    //incriment index on click unless last image, then reset index to 0
-    if ( currImgIndex + 1 < imgSequence.length) { currImgIndex++; } 
-    else { currImgIndex = 0 }
-    
-    //update circle location
-    $(this).animate({
-        "left": imgSequence[currImgIndex][3][1] + 'vw',
-        "top": imgSequence[currImgIndex][3][0] + 'vw'
+//get radius of the circle
+function calculateRadius(){
+    return radius = parseFloat($('.circle').css('height')) / 2;
+};
+
+//takes current screenshot dimesions and the current images x and y percent
+//RETURN: an array [x,y] of the coordinates for the circle 
+//no need for parameters since each image has coordinates associated with it
+//most of this could go in one line, given im going to rewrite it in react im keeping it as minorly convoluted as i can
+//currently going to be top left, will need a radius function to call to offset the position, but thats later
+function calculateCoordinates(){
+    var coordinates = [];
+    var xPercent = imgSequence[currImgIndex][3][0];
+    var yPercent = imgSequence[currImgIndex][3][1];
+    var screenshotWidth = $('.screenshot').width();
+    var screenshotHeight = $('.screenshot').height();
+    var xPosition = (xPercent / 100) * screenshotWidth - calculateRadius();
+    var yPosition = (yPercent / 100) * screenshotHeight - calculateRadius();
+    coordinates.push(xPosition, yPosition);
+    return coordinates;
+}
+
+//sets the x and y coordinates for focus point, seperated out because it was being used twice, on image change and on resize
+function setXY(){
+    $('.focus-point').animate({
+        "left": calculateCoordinates()[0] + 'px',
+        "top": calculateCoordinates()[1] + 'px'
     });
+}
 
-    console.log(document.getElementById('circle-label-id').location);
-    
-    
-    //update image to image at new index
-    var currImage = imgSequence[currImgIndex][0];
-    document.getElementById("screenshot-id").src = currImage;
-    
-    //update label
-    label = imgSequence[currImgIndex][2];
-    document.getElementById('circle-label-text-id').textContent = label;
-    
+//resize
+function updatePositionOnResize(){
+    var resizeTimer;
 
+    function resizeActions(){
+        setXY();
+    };
+
+    //update position when image changes
+    $(window).resize(function(){
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(resizeActions, 250);
+    });
+};
+
+//all actual circle positioning (+label by association, updates all focus point)
+function positionCircle(){
     
-    //test
-    console.log('AFTER CLICK: ' 
-        + '\n' + 'Curent Image Index: ' + currImgIndex 
-        + '\n' + 'Number of Total Images: ' + imgSequence.length
-        + '\n' + 'Current Image: ' + currImage
-        + '\n' + 'Current Label: ' + label
-        + '\n' + 'Current Position: ' + position
-    );
+    updatePositionOnResize();
+
+    $('.focus-point').click(function() {
     
-})
+        //incriment index on click unless last image, then reset index to 0
+        if ( currImgIndex + 1 < imgSequence.length) { currImgIndex++; } 
+        else { currImgIndex = 0 }
+        
+        calculateCoordinates();
+    
+        //update circle location
+        setXY();
+    
+        //update image to image at new index
+        var currImage = imgSequence[currImgIndex][0];
+        document.getElementById("screenshot-id").src = currImage;
+        
+        //update label
+        label = imgSequence[currImgIndex][2];
+        document.getElementById('circle-label-text-id').textContent = label;
+        
+    });
+}
+
+positionCircle();
